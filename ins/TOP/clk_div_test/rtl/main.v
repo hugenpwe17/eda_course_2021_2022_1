c7ee0c77f84da74dd222681c323f940312b122a4
hugenpwe17 <820029885@qq.com>
2021-11-10 21:42:50 +0800

    main

--- ins/TOP/clk_div_test/rtl/main.v
+++ ins/TOP/clk_div_test/rtl/main.v
@@ -5,13 +5,13 @@ module main (
     output clk_div
 );
 
-	clk_div #(
-		.feq_ori	(	5		),
-		.feq_out	( 	1 		)
-	) U1(
-		.clk		(	clk		),
-		.rst_n		(	rst_n	),
-		.clk_div	(	clk_div	)
+	clk_div#(
+		.feq_ori ( 5 		),
+		.feq_out ( 5 		)
+	)u_clk_div(
+		.clk     ( clk     	),
+		.rst_n   ( rst_n   	),
+		.clk_div ( clk_div  )
 	);
 
 endmodule
