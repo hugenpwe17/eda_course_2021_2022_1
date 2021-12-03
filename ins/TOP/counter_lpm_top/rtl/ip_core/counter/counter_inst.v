bbf7ae1dabcd93d6aefe051488d47863123b997c
hugenpwe17 <820029885@qq.com>
2021-12-03 16:54:35 +0800

    main

---
+++ ins/TOP/counter_lpm_top/rtl/ip_core/counter/counter_inst.v
@@ -0,0 +1,9 @@
+counter	counter_inst (
+	.aclr ( aclr_sig ),
+	.clock ( clock_sig ),
+	.cnt_en ( cnt_en_sig ),
+	.data ( data_sig ),
+	.sload ( sload_sig ),
+	.cout ( cout_sig ),
+	.q ( q_sig )
+	);
